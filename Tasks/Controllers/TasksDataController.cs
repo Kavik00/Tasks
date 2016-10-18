9a2386cce61a28147471a766c6810535c120e9a9
Gary Griffeth <gary@griffeth.com>
2016-10-17 18:51:20 -0600

    More Tasks

--- Tasks/Controllers/TasksDataController.cs
+++ Tasks/Controllers/TasksDataController.cs
@@ -11,27 +11,19 @@ namespace Tasks.Controllers
     
     public class TasksDataController : Controller
     {
-        public static List<TaskItem> TasksData = new List<TaskItem>
-        {
-            new TaskItem() {Id=1, Title = "First Task", Completed=false, Created = DateTime.Now, Updated = DateTime.Now, Notes="", ButtonText = "Complete" },
-            new TaskItem() {Id=2, Title = "Second Task", Completed=false, Created = DateTime.Now, Updated = DateTime.Now, Notes="", ButtonText = "Complete" },
-            new TaskItem() {Id=3, Title = "Third Task", Completed=false, Created = DateTime.Now, Updated = DateTime.Now, Notes="", ButtonText = "Complete" },
-            new TaskItem() {Id=4, Title = "Fourth Task", Completed=false, Created = DateTime.Now, Updated = DateTime.Now, Notes="", ButtonText = "Complete" }
-        };
-
         TaskService taskService = new TaskService();
-
-
+        
         [HttpGet("[action]")]
         public IActionResult GetTasks()
         {
-            return Json(TasksData);            
+            return Json(taskService.GetTasks());            
         }
 
         [HttpDelete("[action]/{id}")]
         public IActionResult DeleteTask(int id)
         {
             taskService.DeleteTask(id);
+
             return new NoContentResult();
         }
 
@@ -39,11 +31,61 @@ namespace Tasks.Controllers
         [ProducesResponseType(typeof(TaskItem),201)]
         public IActionResult CreateTask([FromBody] TaskItem task)
         {
-
-            TasksData.Add(task);
-            //"taskService.SaveTasks(TasksData);
+            taskService.AddTask(task);
             return Ok(task);
         }
+    }
+
+
+
+    public class TaskService
+    {
+        private string _baseDirectory = System.AppContext.BaseDirectory;
+        private List<TaskItem> tasksData = new List<TaskItem>();
+        public List<TaskItem> GetTasks()
+        {
+            string json = System.IO.File.ReadAllText(_baseDirectory + "\\tasksData.json");
+            tasksData = JsonConvert.DeserializeObject<List<TaskItem>>(json);
+
+            return tasksData;
+        }
+        //public void SaveTask (TaskItem task)
+        //{
+        //    var tsk = JsonConvert.SerializeObject(task);
+        //    System.IO.File.WriteAllText(_baseDirectory+"tasksData.json", tsk);
+
+        //}
+
+        public void SaveTasks(List<TaskItem> data)
+        {
+            var tsk = JsonConvert.SerializeObject(data);
+            System.IO.File.WriteAllText(_baseDirectory + "\\tasksData.json", tsk);
+
+        }
+
+        public void AddTask(TaskItem task)
+        {
+            var currentTasks = GetTasks();
+            if (currentTasks.Count() > 0)
+            {
+                var maxId = currentTasks.Where(t => t.Id == currentTasks.Max(m => m.Id)).FirstOrDefault().Id;
+                task.Id = maxId + 1;
+            }
+            else task.Id = 1;
+
+            tasksData.Add(task);
+            SaveTasks(tasksData);
+
+        }
+
+        public void DeleteTask(int id)
+        {
+            var currentTasks = GetTasks();
+            var toDelete = currentTasks.Find(t => t.Id == id);
+            currentTasks.Remove(toDelete);
+            SaveTasks(currentTasks);   
+        }
+
     }
 
     public class TaskItem
@@ -56,31 +98,15 @@ namespace Tasks.Controllers
         public string Notes { get; set; }
         public string ButtonText { get; set; }
 
+
+        public static List<TaskItem> TasksData = new List<TaskItem>
+        {
+            new TaskItem() {Id=1, Title = "First Task", Completed=false, Created = DateTime.Now, Updated = DateTime.Now, Notes="", ButtonText = "Complete" },
+            new TaskItem() {Id=2, Title = "Second Task", Completed=false, Created = DateTime.Now, Updated = DateTime.Now, Notes="", ButtonText = "Complete" },
+            new TaskItem() {Id=3, Title = "Third Task", Completed=false, Created = DateTime.Now, Updated = DateTime.Now, Notes="", ButtonText = "Complete" },
+            new TaskItem() {Id=4, Title = "Fourth Task", Completed=false, Created = DateTime.Now, Updated = DateTime.Now, Notes="", ButtonText = "Complete" }
+        };
     }
 
-    public class TaskService
-    {
-        public void SaveTask (TaskItem task)
-        {
-            var tsk = JsonConvert.SerializeObject(task);
-            System.IO.File.WriteAllText(@"C:\Users\gary\Source\Repos\Tasks\Tasks\Tasks\tasksData.json", tsk);
 
-        }
-
-        public void SaveTasks(List<TaskItem> data)
-        {
-            var tsk = JsonConvert.SerializeObject(data);
-            System.IO.File.WriteAllText(@"C:\Users\gary\Source\Repos\Tasks\Tasks\Tasks\tasksData.json", tsk);
-
-        }
-
-        public void DeleteTask(int id)
-        {
-           var toDelete = TasksDataController.TasksData.Find(t => t.Id == id);
-            TasksDataController.TasksData.Remove(toDelete);
-
-               
-        }
-
-    }
 }
